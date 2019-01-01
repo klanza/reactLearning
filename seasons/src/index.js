0cae042eb9e4eb1ceb7e5a6d1167f4fafe17ea63
klanza <klanza91@gmail.com>
2019-01-01 13:28:16 -0700

    Working display of season based on geolocation

--- seasons/src/index.js
+++ seasons/src/index.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
+import SeasonDisplay from './SeasonDisplay';
 
 class App extends Component {
   // THIS CODE ALLOWS US TO SKIP CONSTRUCTOR AND SUPER
@@ -20,7 +21,7 @@ class App extends Component {
       return <div>Error: {this.state.errorMessage}</div>;
     }
     if (!this.state.errorMessage && this.state.lat) {
-      return <div>Latitude: {this.state.lat}</div>;
+      return <SeasonDisplay lat={this.state.lat} />;
     }
 
     return <div>Loading...</div>;
