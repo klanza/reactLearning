69a37732a44286c2e0e73a32da90433d48ab2425
klanza <klanza91@gmail.com>
2019-01-03 15:30:57 -0700

    Fix error with undefined form submit

--- pics/src/components/SearchBar.js
+++ pics/src/components/SearchBar.js
@@ -3,10 +3,22 @@ import React, { Component } from 'react';
 class SearchBar extends Component {
   state = { term: '' };
 
+  // IF THIS IS NOT AN ARROW FUNCTION
+  // Lacks the binding of this
+  // IN CONSTRUCT, CORRECT WITH: this.onFormSubmit = this.onFormSubmit.bind(this)
+  onFormSubmit = event => {
+    event.preventDefault();
+    console.log(this.state.term);
+  };
+
   render() {
     return (
       <div className="ui segment">
-        <form className="ui form">
+        {/* IF ERROR WITH 'Cannot find property this of undefined
+          Change to arrow function in property, add parentheses at end of function
+          to invoke function when form is submitted 
+          {event => this.onFormSubmit(event) */}
+        <form onSubmit={this.onFormSubmit} className="ui form">
           <div className="field">
             <label>Image Search</label>
             <input
