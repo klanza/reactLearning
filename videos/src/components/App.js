02ca7ea8f46e721c7e1645a95d0419980e65c7fb
klanza <klanza91@gmail.com>
2019-01-08 12:27:50 -0700

    Refactoring and finishing touches

--- videos/src/components/App.js
+++ videos/src/components/App.js
@@ -8,14 +8,20 @@ import youtube from '../apis/youtube';
 class App extends Component {
   state = { videos: [], selectedVideo: null };
 
+  componentDidMount() {
+    this.onTermSubmit('buildings');
+  }
+
   onTermSubmit = async term => {
     const response = await youtube.get('/search', {
       params: {
         q: term
       }
     });
-    this.setState({ videos: response.data.items });
-    console.log(response.data);
+    this.setState({
+      videos: response.data.items,
+      selectedVideo: response.data.items[0]
+    });
   };
 
   onVideoSelect = video => {
