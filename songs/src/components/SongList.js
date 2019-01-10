45d1ec38bf27ce7320e376d9ec410c06a5060af8
klanza <klanza91@gmail.com>
2019-01-09 21:03:27 -0700

    Additional building of component, semantic styling

--- songs/src/components/SongList.js
+++ songs/src/components/SongList.js
@@ -2,8 +2,20 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 
 class SongList extends Component {
+  renderList() {
+    return this.props.songs.map(song => {
+      return (
+        <div className="item" key={song.title}>
+          <div className="right floated content">
+            <button className="ui button primary">Select</button>
+          </div>
+          <div className="content">{song.title}</div>
+        </div>
+      );
+    });
+  }
   render() {
-    return <div>SongList</div>;
+    return <div className="ui divided list">{this.renderList()}</div>;
   }
 }
 
@@ -14,4 +26,7 @@ const mapStateToProps = state => {
 };
 
 // Due to connect having another function inside of it, takes the SongList as an argument
+// REQUIRED:
+// Import connect, define mapStateToProps, wire connect with mapStateToProps as first
+// argument, and the component as the second argument
 export default connect(mapStateToProps)(SongList);
