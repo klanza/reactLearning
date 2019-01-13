9cced84e82f5e443a82342337c402e2dcb2db5b5
klanza <klanza91@gmail.com>
2019-01-12 17:50:00 -0700

    Work on reducers for post fetching

--- blog/src/reducers/index.js
+++ blog/src/reducers/index.js
@@ -1,5 +1,7 @@
 import { combineReducers } from 'redux';
 
+import postsReducer from './postsReducer';
+
 export default combineReducers({
-  replaceMe: () => 'Replace when building reducers'
+  posts: postsReducer
 });
