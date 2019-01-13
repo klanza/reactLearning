f48b9c7691abe6350322b1f87b381eb80295c495
klanza <klanza91@gmail.com>
2019-01-12 18:45:33 -0700

    Add styling, and author to user header component, refactor reducers

---
+++ blog/src/components/UserHeader.js
@@ -0,0 +1,29 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { fetchUser } from '../actions';
+
+class UserHeader extends Component {
+  componentDidMount() {
+    this.props.fetchUser(this.props.userId);
+  }
+
+  render() {
+    const { user } = this.props;
+    if (!user) {
+      return null;
+    }
+
+    return <div className="header">{user.name}</div>;
+  }
+}
+
+// Can use ownProps is an object that is a reference to the props
+// sent to accompanying component
+const mapStateToProps = (state, ownProps) => {
+  return { user: state.users.find(user => user.id === ownProps.userId) };
+};
+
+export default connect(
+  mapStateToProps,
+  { fetchUser }
+)(UserHeader);
